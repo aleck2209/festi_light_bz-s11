3bc0b85c09decbd9639ce859d7d0e1edf2f3f4b3
aleck2209 <aleckyann09@gmail.com>
2026-08-19 17:26:48 +0100

    feat: Realisation de la hero section et du compte a rebours

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const navBar = document.getElementById("nav-bar");
 const btnHamburger = document.getElementById("btn-hamburger");
 const btnClose = document.getElementById("btn-close");
 const navBarLinks = document.querySelectorAll(".nav-bar a");
+// Compte a rebour
+const chrono = document.getElementById("chrono");
+let dateFestival = "2026-08-22T18:00:00";
 
 // Fonctions
 // Menu hamburger
@@ -29,7 +32,73 @@ const menuHamburger = () => {
 		});
 	});
 };
+// Compte a rebour
+// calculer le temps restant
+const timeStay = (date) => {
+	const dateNow = new Date();
+	const dateTarget = new Date(date);
+	// Caluculer la difference
+	const difference = dateTarget - dateNow;
+	if (difference <= 0) {
+		return {
+			day: 0,
+			hour: 0,
+			minute: 0,
+			second: 0,
+		};
+	}
+	// Total des seconde restantes
+	const secondTotal = Math.floor(difference / 1000);
+	// Nombre de jours restants
+	const day = Math.floor(secondTotal / 86400);
+	// Nombre d'heure
+	const hour = Math.floor((secondTotal % 86400) / 3600)
+	// Nombre de minute
+	const minute = Math.floor((secondTotal % 3600 ) / 60);
+	// Nombre de seconde
+	const second = Math.floor((secondTotal % 60));
+
+	return {
+		day,
+		hour,
+		minute,
+		second
+	}
+};
+// Afficher le temps
+const displayTime = () => {
+	const time = timeStay(dateFestival);
+	if (time) {
+		const day = String(time.day).padStart(2, "0");
+		const hour = String(time.hour).padStart(2, "0");
+		const minute = String(time.minute).padStart(2, "0");
+		const second = String(time.second).padStart(2, "0");
+		chrono.innerHTML = `
+			<div class="time">
+				<time>${day}</time>
+				<span>Jours</span>
+			</div>
+			<div class="time">
+				<time>${hour}</time>
+				<span>Heures</span>
+			</div>
+			<div class="time">
+				<time>${minute}</time>
+				<span>Minutes</span>
+			</div>
+			<div class="time">
+				<time>${second}</time>
+				<span>Secondes</span>
+			</div>
+		`
+	}
+}
 
 // Appel des fonction
-// MenuHamber
+// Menu hamburger
 menuHamburger();
+
+// compte a rebour
+displayTime();
+
+setInterval(displayTime, 1000)
